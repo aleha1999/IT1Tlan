8e436f254724e263e5185e411def8f4114465082
Alexander Hansen <alexander@trianite.com>
2018-04-20 22:54:09 +0200

    Made finish static

--- src/php/classes/APIFrame.php
+++ src/php/classes/APIFrame.php
@@ -1,6 +1,6 @@
 <?php
 class APIFrame {
-    function finish($success,$data = array(),$key = "data") {
+    public static function finish($success,$data = array(),$key = "data") {
         header("Content-Type: application/json");
         die(json_encode(array("success"=>$success,$key=>$data)));
     }
